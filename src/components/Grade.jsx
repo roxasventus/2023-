7b72b9ce3e86bdc4264f22224f7029e78f01c8c1
Mungwani <gwcowani@kangwon.ac.kr>
2023-01-18 20:07:01 +0900

    hoho

--- src/components/Grade.jsx
+++ src/components/Grade.jsx
@@ -4,8 +4,8 @@ import "./GradeCSS.css"
 export default function Grade(props){
     return( 
         <>
-            <div style={{height:"229px", width: "829px", display:"flex", margin:"118px auto"}}>
-                <img style={{width: "585px", height: "297px"}} alt="안됨" src={props.img}></img>
+            <div style={{height:"auto", width: "55%", display:"flex", margin:"118px auto"}}>
+                <img style={{width: "55%", height: "35%"}} alt="안됨" src={props.img}></img>
                 <div className="GWrapper">
                     <div  className="GradeTitle">
                         <div
@@ -18,7 +18,7 @@ export default function Grade(props){
                                 fontFamily: 'Gmarket Sans TTF',
                                 fontStyle: "normal",
                                 fontWeight: 700,
-                                fontSize: "40px",
+                                fontSize: "3rem",
                                 lineHeight: "46px",
                                 
                                 color: "#000000",
@@ -38,15 +38,15 @@ export default function Grade(props){
                         
                         margin:"28px 53px",
 
-                        width: "337px",
-                        height: "187px",
+                        width: "90%",
+                        height: "auto",
                         left: "991px",
                         top: "1833px",
                         
                         fontFamily: 'Gothic A1',
                         fontStyle: "normal",
                         fontWeight: 500,
-                        fontSize: "16px",
+                        fontSize: "1rem",
                         lineHeight: "25px",
                         /* or 156% */
                         
