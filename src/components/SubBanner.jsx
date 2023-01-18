254df67006a641ece4318082d022c7e93ab433ab
01star01ek <01star01ek@gmail.com>
2023-01-18 10:11:38 +0900

    hi

--- src/components/SubBanner.jsx
+++ src/components/SubBanner.jsx
@@ -11,14 +11,14 @@ export default function SubBanner(props) {
         fontFamily: 'Gmarket Sans TTF',
         fontStyle: "normal",
         fontWeight: 700,
-        fontSize: "3rem",
+        fontSize: "2.5rem",
         lineHeight: "55px",
         textAlign: "center",
         
         color: "#000000"
       }}>{props.text}</div>
       <hr style={{
-        width: "10%",
+        width: "150px",
         height: "10px",
         background: "#719C17",
         borderRadius: "4px"
