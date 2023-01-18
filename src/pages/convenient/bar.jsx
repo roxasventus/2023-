35db719a7afd7ebb67b4b1e8dcbbc6ae10e42310
Sonny <huyb20s@naver.com>
2023-01-18 12:24:18 +0900

    ADD culture bottom h-scroll

--- src/pages/convenient/bar.jsx
+++ src/pages/convenient/bar.jsx
@@ -5,18 +5,38 @@ import Grade from '../../components/Grade';
 export default function Bar(props) {
   return (
     <>
-      <SubBanner text="술집"/>
-      <Grade title="Kelvin Hotel" 
-      text="20 Kelvin Street, Invercargill 9810032182829"
+      <SubBanner text="주류 백화점"/>
+      <Grade title="Liquorland South City" 
+      text="Cnr Elles Rd &, Tweed Street, 
+      West Invercargill, Invercargill 9810
+      032189729"
       url="https://www.kelvinhotel.co.nz/"
+      img="../image/image 17.png"
       />
-            <Grade title="Kelvin Hotel" 
-      text="20 Kelvin Street, Invercargill 9810032182829"
+            <Grade title=" Super Liquor Southland" 
+      text=" 406 Elles Road, Kew, Invercargill 9879
+      032165329"
       url="https://www.kelvinhotel.co.nz/"
+      img="../image/image 18.png"
       />
-            <Grade title="Kelvin Hotel" 
-      text="20 Kelvin Street, Invercargill 9810032182829"
+            <Grade title="East End Liquor" 
+      text=" 12 Bamborough Street, Richmond, Invercargill 9810
+      032173415"
       url="https://www.kelvinhotel.co.nz/"
+      img="../image/image 19.png"
+      />
+      <Grade title="Liquorland Centrepoint" 
+      text=" 252 Dee Street, Avenal, Invercargill 9810
+      032113650"
+      url="https://www.kelvinhotel.co.nz/"
+      img="../image/image 20.png"
+      />
+      <Grade title=" Super Liquor Collingwood" 
+      text=" 44 North Road, Prestonville, 
+      Invercargill 9810
+      032158308"
+      url="https://www.kelvinhotel.co.nz/"
+      img="../image/image 21.png"
       />
     </>
   );
